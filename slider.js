26f23ed176556f3b8ac8f1b028c1147ab0ee43c8
Praharsh Singh <praharsh535@gmail.com>
2021-06-07 22:02:04 +0530

    last to first spacing fix

--- slider.js
+++ slider.js
@@ -3,7 +3,7 @@ $(".slider1").slick({
   arrows: false,
   infinite: true,
   speed: 300,
-  slidesToShow: 3,
+  slidesToShow: 7,
   slidesToScroll: 1,
   responsive: [
     {
@@ -38,13 +38,14 @@ $(document).ready(function () {
     arrows: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 5,
+    slidesToShow: 7,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1250,
         settings: {
           slidesToShow: 2,
+          infinite: true,
           slidesToScroll: 1,
         },
       },
@@ -52,6 +53,7 @@ $(document).ready(function () {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
+          infinite: true,
           slidesToScroll: 1,
         },
       },
@@ -59,12 +61,10 @@ $(document).ready(function () {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
+          infinite: true,
           slidesToScroll: 1,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 });
@@ -72,7 +72,7 @@ $(".slider1 .card").hover(function (index1, value1) {
   $(this).css("opacity", "1");
   $(this).css("transform", "scale(1.05)");
   var current = this;
-  $(".slider1 .card").each(function (index) {
+  $(".card").each(function (index) {
     if (this !== current) {
       $(this).css("opacity", "0.5");
       $(this).css("transform", "scale(1)");
@@ -83,7 +83,7 @@ $(".slider2 .card").hover(function (index1, value1) {
   $(this).css("opacity", "1");
   $(this).css("transform", "scale(1.05)");
   var current = this;
-  $(".slider2 .card").each(function (index) {
+  $(".card").each(function (index) {
     if (this !== current) {
       $(this).css("opacity", "0.5");
       $(this).css("transform", "scale(1)");
