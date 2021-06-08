7c06c1ca496ff927fc8f7a7fd9fe61d02b87ac76
Praharsh Singh <praharsh535@gmail.com>
2021-06-08 15:40:51 +0530

    pause on focus

--- slider.js
+++ slider.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   $(".slider1").slick({
     autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
     autoplaySpeed: 2000,
     dots: false,
     arrows: false,
@@ -39,6 +41,8 @@ $(document).ready(function () {
     ],
   });
   $(".slider2").slick({
+    pauseOnHover: false,
+    pauseOnFocus: false,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: false,
